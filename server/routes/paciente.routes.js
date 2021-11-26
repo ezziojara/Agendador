d854fe39c1ec8f6d8cbc9bde8550e82d80e1948a
Ezzio Jara <ejara@MacBook-Pro-de-Ezzio.local>
2021-11-25 21:35:36 -0300

    Quitar token

--- server/routes/paciente.routes.js
+++ server/routes/paciente.routes.js
@@ -1,8 +1,8 @@
 const {getPacientexRut, createPaciente, UpdatePaciente} = require('../controllers/paciente.controller');
-const validaJWT = require('../middlewares/valida-jwt');
+
 
 module.exports = (app) => {
-    app.get('/api/pacientes/:rut', validaJWT ,getPacientexRut);
-    app.post('/api/pacientes/new', validaJWT ,createPaciente);
-    app.put('/api/pacientes/update/:id', validaJWT ,UpdatePaciente);
+    app.get('/api/pacientes/:rut',getPacientexRut);
+    app.post('/api/pacientes/new',createPaciente);
+    app.put('/api/pacientes/update/:id',UpdatePaciente);
 }
--- server/routes/reserva.routes.js
+++ server/routes/reserva.routes.js
@@ -3,7 +3,7 @@ const validaJWT = require('../middlewares/valida-jwt');
 
 module.exports = (app) => {
     app.get('/api/reservas/:id:fecha', validaJWT ,getReservaxDoctorxFecha);
-    app.post('/api/reservas/new', validaJWT ,createReserva);
-    app.put('/api/reservas/update/:id', validaJWT ,UpdateReserva);
-    app.delete('/api/reservas/delete/:id', validaJWT ,deleteReserva);
+    app.post('/api/reservas/new' ,createReserva);
+    app.put('/api/reservas/update/:id' ,UpdateReserva);
+    app.delete('/api/reservas/delete/:id' ,deleteReserva);
 }

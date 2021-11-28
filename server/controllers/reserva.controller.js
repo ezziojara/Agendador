17315ae6cfa2dc90854f772177feb382a0b2a9ae
Ezzio Jara <ejara@MacBook-Pro-de-Ezzio.local>
2021-11-27 21:17:32 -0300

    Buscar doctor por fecha y devuelve si esta disponible el horario

--- server/controllers/reserva.controller.js
+++ server/controllers/reserva.controller.js
@@ -1,12 +1,101 @@
 const Reserva = require('../models/reserva.model');
+const Horario = require('../models/horario.model');
+var mongoose = require('mongoose');
 
 module.exports.getReservaxDoctorxFecha = async (req, res) => {
     try{
         let { id, fecha } = req.params;
+        const horarios = [];
         fecha = new Date(fecha);
-        const reservaList = await Reserva.find({ doctor: id, fecha: fecha });
+        id = mongoose.Types.ObjectId(id);
+        console.log(id)
+
+        let horarioList = await Horario.aggregate([
+            
+            { 
+                $lookup: 
+                { 
+                    from: 'reservas',
+                    localField: '_id',
+                    foreignField: 'horario',
+                    as: 'horarios'
+                } 
+            }, 
+            {
+              $project: 
+                {
+                    _id: 1,
+                    orden:1,
+                    horaInicio:1,
+                    horaFin:1,
+                    estado:1,
+                    horarios: 
+                    { 
+                    $filter: 
+                    { 
+                        input: "$horarios", 
+                        as: "pet", 
+                        cond: {
+                            $and: [
+                                    {
+                                    $in: [
+                                        "$$pet.doctor",
+                                        [id]
+                                    ]
+                                    },
+                                    { $eq: ["$$pet.fecha", fecha] }
+                                    ]
+                        
+                        }
+                    } 
+                    } 
+                }
+            } , 
+            {
+              $project: 
+              {
+                _id:1,
+                orden:1,
+                horaInicio:1,
+                horaFin:1,
+                estado:1,
+                tieneReserva:{$size:'$horarios'}
+              }
+            }  
+            
+            
+        ]);
+
         
-        return res.json( reservaList );
+
+        // horarioList = horarioList.find({})
+
+        // const reservaList = await Reserva.find({ doctor: id, fecha: fecha });
+        // const horarioList = await Horario.find();
+
+        // horarioList?.map((horario)=>{
+        //     reservaList?.map((reserva)=>{
+        //         console.log("reserva.horario",reserva.horario)
+        //         if(horario._id.toString() === reserva.horario.toString()){
+        //             horario = { 
+        //                         "_id": horario._id,
+        //                         "orden": horario.orden,
+        //                         "horaInicio": horario.horaInicio,
+        //                         "horaFin": horario.horaFin,
+        //                         "estado": horario.estado,
+        //                         "disponible": false
+        //                       }
+        //         }
+                
+                
+        //         horarios.push(horario)
+        //     })
+            
+        // })
+
+        // console.log('horarios',horarios);
+
+        return res.json( horarioList );
     }catch(err){
         console.log("err",err)
         return res.status(500).json({error: err});
